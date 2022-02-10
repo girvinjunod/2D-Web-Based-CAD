44829ce8ab8193fe5c8bfe4eade2f1e6007213d4
girvinjunod <girvinjunod@gmail.com>
2022-02-10 22:49:48 +0700

    feat: add help button + modal

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-function main() {
-    const canvas = document.querySelector("#glCanvas");
-    // Initialize the GL context
-    const gl = canvas.getContext("webgl");
+const canvas = document.querySelector("#glCanvas");
+const gl = canvas.getContext("webgl");
 
+const resizeCanvas = (gl) => {
+    gl.canvas.width = (9 / 12) * window.innerWidth;
+    gl.canvas.height = (9 / 12) * window.innerWidth;
+};
+
+window.onload = function main() {
     // Only continue if WebGL is available and working
     if (gl === null) {
         alert("Unable to initialize WebGL. Your browser or machine may not support it.");
         return;
     }
+    resizeCanvas(gl);
+    window.addEventListener("resize", () => resizeCanvas(gl), false);
 
-    // Set clear color to black, fully opaque
-    gl.clearColor(0, 0.0, 0.0, 1.0);
-    // Clear the color buffer with specified clear color
+    gl.clearColor(0.6, 0.75, 0.9, 1);
+    
     gl.clear(gl.COLOR_BUFFER_BIT);
-    }
-
-    window.onload = main;
+}
