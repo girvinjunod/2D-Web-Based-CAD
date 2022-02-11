05c6e17e96aaf9d991a85c23512c8f281bd1047b
Dionisius Darryl Hermansyah <dionisius.dh28@gmail.com>
2022-02-11 08:24:08 +0700

    feat(Options): add shape selector, size slider; chore: format with prettier

--- script.js
+++ script.js
@@ -2,20 +2,22 @@ const canvas = document.querySelector("#glCanvas");
 const gl = canvas.getContext("webgl");
 
 const resizeCanvas = (gl) => {
-    gl.canvas.width = (9 / 12) * window.innerWidth;
-    gl.canvas.height = (9 / 12) * window.innerWidth;
+  gl.canvas.width = (9 / 12) * window.innerWidth;
+  gl.canvas.height = (9 / 12) * window.innerWidth;
 };
 
 window.onload = function main() {
-    // Only continue if WebGL is available and working
-    if (gl === null) {
-        alert("Unable to initialize WebGL. Your browser or machine may not support it.");
-        return;
-    }
-    resizeCanvas(gl);
-    window.addEventListener("resize", () => resizeCanvas(gl), false);
+  // Only continue if WebGL is available and working
+  if (gl === null) {
+    alert(
+      "Unable to initialize WebGL. Your browser or machine may not support it."
+    );
+    return;
+  }
+  resizeCanvas(gl);
+  window.addEventListener("resize", () => resizeCanvas(gl), false);
 
-    gl.clearColor(0.6, 0.75, 0.9, 1);
-    
-    gl.clear(gl.COLOR_BUFFER_BIT);
-}
+  gl.clearColor(0.6, 0.75, 0.9, 1);
+
+  gl.clear(gl.COLOR_BUFFER_BIT);
+};
