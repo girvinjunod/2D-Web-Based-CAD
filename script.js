d5a9b1985696720560150cd0178749437618c23c
Dionisius Darryl Hermansyah <dionisius.dh28@gmail.com>
2022-02-11 11:59:57 +0700

    chore: add prettier formatter config

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const canvas = document.querySelector("#glCanvas");
-const gl = canvas.getContext("webgl");
+const canvas = document.querySelector('#glCanvas')
+const gl = canvas.getContext('webgl')
 
 const resizeCanvas = (gl) => {
-  gl.canvas.width = (9 / 12) * window.innerWidth;
-  gl.canvas.height = (9 / 12) * window.innerWidth;
-};
+  gl.canvas.width = (9 / 12) * window.innerWidth
+  gl.canvas.height = (9 / 12) * window.innerWidth
+}
 
 window.onload = function main() {
   // Only continue if WebGL is available and working
   if (gl === null) {
     alert(
-      "Unable to initialize WebGL. Your browser or machine may not support it."
-    );
-    return;
+      'Unable to initialize WebGL. Your browser or machine may not support it.'
+    )
+    return
   }
-  resizeCanvas(gl);
-  window.addEventListener("resize", () => resizeCanvas(gl), false);
+  resizeCanvas(gl)
+  window.addEventListener('resize', () => resizeCanvas(gl), false)
 
-  gl.clearColor(0.6, 0.75, 0.9, 1);
+  gl.clearColor(0.6, 0.75, 0.9, 1)
 
-  gl.clear(gl.COLOR_BUFFER_BIT);
-};
+  gl.clear(gl.COLOR_BUFFER_BIT)
+}
